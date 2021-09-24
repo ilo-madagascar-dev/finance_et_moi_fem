9e7ed4dfa771644393b454f0ac8b16ee53e2904d
Ilo_Madagascar <henintsoa.rafidy@gmail.com>
2021-09-24 07:50:48 +0200

    iframe .env .env.local

--- src/Controller/AdminSystemController.php
+++ src/Controller/AdminSystemController.php
@@ -22,14 +22,8 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+// use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-
-/**
-* Require ROLE_ADMIN for *every* controller method in this class.
-*
-* @IsGranted("ROLE_ADMIN")
-*/
 
 class AdminSystemController extends AbstractController
 {
