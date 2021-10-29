5083c9be1867dd0b03cc943f30e3569509a75dd8
Ilo_Madagascar <henintsoa.rafidy@gmail.com>
2021-10-29 07:57:58 +0300

    Récupération de tous les abonnements

--- src/Controller/AdminSubscriptionsListAndFiltersController.php
+++ src/Controller/AdminSubscriptionsListAndFiltersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AbonnementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,13 @@ class AdminSubscriptionsListAndFiltersController extends AbstractController
     /**
      * @Route("/admin/subscription/list/and/filters", name="admin_subscription_list_and_filters")
      */
-    public function index(): Response
+    public function index(AbonnementRepository $abonnementRepository): Response
     {
+        $everyAbonnement = $abonnementRepository->findAll();
+
         return $this->render('admin_subscriptions_list_and_filters/index.html.twig', [
             'controller_name' => 'AdminSubscriptionsListAndFiltersController',
+            'abonnements' => $everyAbonnement
         ]);
     }
 }
